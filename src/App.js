c912515c7687144833557c677d9b0b8446d53bb6
Otto Schmid <otto.schmid@Ottos-MacBook-Pro.local>
2020-05-05 18:00:45 +0200

    Done.

--- src/App.js
+++ src/App.js
@@ -11,85 +11,16 @@ function App() {
         </a>
         <p className="logo-text">VEB KÜHLAUTOMAT</p>
         <a className="btn btn-outline-light btn-sm" href="/login">
-          Log Out
+          Login
         </a>
       </nav>
 
-      <header className="masthead text-white text-center">
-        <div className="container profile-welcome mt-5">
-          <h4>Profileinstellungen</h4>
-          <hr className="my-4" />
-          <p className="lead">Account Nr.: 0233422094</p>
-          <small className="marine-text">Eintrittsdatum: 23.03.1987</small>
-          <br />
-        </div>
-      </header>
-      <section>
-        <div className="jumbotron" style={{ padding: '20px', margin: '20px' }}>
-          <form>
-            <div className="form-row">
-              <div className="form-group col-md-6">
-                <label>Firmenname</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="fn1"
-                  placeholder="VE Werft Stralsund BT Beschaffung Prohn"
-                />
-              </div>
-            </div>
-            <div className="form-row">
-              <div className="form-group col-md-6">
-                <label>Email</label>
-                <input type="email" className="form-control" id="inputEmail4" />
-              </div>
-              <div className="form-group col-md-6">
-                <label>Passwort</label>
-                <input type="password" className="form-control" id="inputPassword4" />
-              </div>
-            </div>
-            <div className="form-group">
-              <label>Anschrift (1)</label>
-              <input type="text" className="form-control" id="inputAddress" />
-            </div>
-            <div className="form-group">
-              <label>Anschrift (2)</label>
-              <input type="text" className="form-control" id="inputAddress2" />
-            </div>
-            <div className="form-row">
-              <div className="form-group col-md-2">
-                <label>Postleitzahl</label>
-                <input type="text" className="form-control" id="inputZip" />
-              </div>
-              <div className="form-group col-md-4">
-                <label>Land</label>
-                <select className="form-control" id="ctrySelect">
-                  <option>DDR</option>
-                  <option>BRD</option>
-                  <option>VR Polen</option>
-                  <option>CSSR</option>
-                  <option>SU</option>
-                </select>
-              </div>
-            </div>
-            <hr />
-            <button type="submit" className="btn btn-outline-dark btn-sm ">
-              Änderungen speichern
-            </button>
-          </form>
-        </div>
-      </section>
-      <section className="showcase">
-        <div className="jumbotron" style={{ padding: '20px', margin: '0px 20px 20px 20px' }}>
-          <small>
-            Sie können hier die Löschung Ihres Profils beantragen. Warnung! Dies kann nicht rückgängig gemacht werden!
-          </small>
-          <br />
-          <a className="btn btn-outline btn-sm mt-2 reddish-button" href="#/edit-profile" role="button">
-            Profil löschen
-          </a>
-        </div>
-      </section>
+      <div className="jumbotron mt-5">
+  <h1 className="display-4">404 Page Not Found</h1>
+  <p className="lead">Die von Ihnen angeforderte Seite ist nicht verfügbar. Wir bedauern dies.</p>
+  <hr className="my-4"/>
+  <a class="btn btn-secondary btn-sm" href="https://kuelautomat.com" role="button">Zurück zur Homepage</a>
+</div>
 
       <footer className="footer marine" style={{ paddingBottom: '20px' }}>
         <div className="container marine">
